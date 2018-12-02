fe24eb6d98ce9be7a5d79e1605cf2202d6bad5d6
Babak Khorrami <k.babak@gmail.com>
2018-12-03 01:45:43 +0330

    add item form submission (call api)

--- src/app/components/items-add.js
+++ src/app/components/items-add.js
@@ -2,6 +2,16 @@ angular
   .module('app')
   .component('addItems', {
 	templateUrl: 'app/components/items-add.html',
-	controller: function () {
+	controller: function ($scope, $state, $http) {
+		this.submit = function () {
+			$http.post("/api/items", {
+				name: $scope.name,
+				price: $scope.price,
+				img: $scope.image,
+			})
+			.then(function(res){
+				// res.data;
+			});
+		}
 	}
   });
