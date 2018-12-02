c4082a8226fc5a864da4921e6da416a14934b84b
Babak Khorrami <k.babak@gmail.com>
2018-12-03 02:53:30 +0330

    toast after add to cart

--- src/app/components/items.js
+++ src/app/components/items.js
@@ -2,7 +2,7 @@ angular
   .module('app')
   .component('items', {
 	templateUrl: 'app/components/items.html',
-	controller: function ($scope, $state, $http, cartStorage) {
+	controller: function ($scope, $state, $http, cartStorage, ngToast) {
 		$http.get("/api/items")
 		.then(function(res){
 			$scope.items = res.data;
@@ -11,6 +11,7 @@ angular
 		$scope.addToCart = function (item) {
 			console.log("add to cart:", item);
 			cartStorage.addItem(item);
+			ngToast.create('Item added to cart!');
 		}
 	}
   });
