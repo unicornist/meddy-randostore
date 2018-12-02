0acb15dd6e5c2f2a447f8f27bfa84300ad03160c
Babak Khorrami <k.babak@gmail.com>
2018-12-03 01:37:56 +0330

    call api and show items

--- src/app/components/items.js
+++ src/app/components/items.js
@@ -2,6 +2,14 @@ angular
   .module('app')
   .component('items', {
 	templateUrl: 'app/components/items.html',
-	controller: function () {
+	controller: function ($scope, $state, $http) {
+		$http.get("/api/items")
+		.then(function(res){
+			$scope.items = res.data;
+		});
+
+		$scope.addToCart = function (item) {
+			console.log("add to cart:", item);
+		}
 	}
   });
