2db755bf628a6c0a5f5327346dde7e352bf7f578
Babak Khorrami <k.babak@gmail.com>
2018-12-03 03:56:00 +0330

    header navbar with cart count

--- src/app/components/site.js
+++ src/app/components/site.js
@@ -2,7 +2,9 @@ angular
   .module('app')
   .component('site', {
 	templateUrl: 'app/components/site.html',
-	controller: function ($scope, $state){
-
+	controller: function ($scope, $state, cartStorage){
+		this.getCartCount = function(){
+			return cartStorage.getCount();
+		}
 	}
   });
